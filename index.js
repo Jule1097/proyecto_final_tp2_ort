885e5ef89fe41cc3997c323508c7843a0a4a4507
Julian Ibarra <97622224+Jule1097@users.noreply.github.com>
2025-11-26 20:57:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const runServer = async () => {
   const PORT = process.env.PORT || 3000;
   
   server.listen(PORT, () => {
-    console.log(Servidor corriendo en el puerto ${PORT});
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
   });
 };
